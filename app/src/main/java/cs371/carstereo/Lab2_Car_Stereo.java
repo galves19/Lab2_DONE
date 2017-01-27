58d1aba0c938549bd381eebf6a88aadce6a51ae3
galves19 <galves19@up.edu>
2017-01-26 18:44:35 -0800

    Initial commit

---
+++ app/src/main/java/cs371/carstereo/Lab2_Car_Stereo.java
@@ -0,0 +1,13 @@
+package cs371.carstereo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lab2_Car_Stereo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab2__car__stereo);
+    }
+}
